30f914f04dcfa67fdbd1e2eadd8c8ba97547b91b
ahmad <ahmad.tohan92@gmail.com>
2024-10-18 18:37:01 +0400

    ok

--- src/main/java/com/top/lcd/Application.java
+++ src/main/java/com/top/lcd/Application.java
@@ -13,7 +13,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 public class Application {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
